b0c8e3e9451d126646209de7a0fbb564b8fa20b8
Rafaël Daigneau <rafael.daigneau@gmail.com>
2021-12-03 15:16:08 +0100

    Preparing for deployment to Vercel

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Layout from "components/Layout"
+
+
+
+function About() {
+    return (
+       <>    
+        <Layout>
+            <h1>I am about page</h1>
+        </Layout>
+       </> 
+    )
+    
+}
+
+export default About
